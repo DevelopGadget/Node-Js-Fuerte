7adfec3ccccddfa560d8b6fc0020f6dc3c1b0cf1
Fernando Araujo <developgadget@gmail.com>
2019-06-22 16:36:36 -0500

    Socket init

---
+++ Chat-Socket/Main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+//Puerto
+app.set('port', process.env.PORT || 3000);
+
+//Uses
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./Routes/Hero.routes').HeroRoutes(app);
+
+//Listen
+app.listen(app.get('port'), () => {
+    console.log('Init')
+
+});
