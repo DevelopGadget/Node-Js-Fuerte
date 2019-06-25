193ed06115123119f650b155905c7dd515e5c841
Fernando Araujo <developgadget@gmail.com>
2019-06-25 11:53:50 -0500

    Card

--- Chat-Socket/Main.js
+++ Chat-Socket/Main.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-require('./Routes/User.routes').User(app);
-
 //Listen
 const server = app.listen(app.get('port'), () => {
     console.log('Init');
--- Chat-Socket/Routes/User.routes.js
+++
@@ -1,9 +0,0 @@
-const UserController = require('../Controllers/User.controller');
-
-exports.User = app => {
-    app.get('/User', UserController.Get);
-    app.get('/User/:Id', UserController.GetById);
-    app.post('/User', UserController.Post);
-    app.put('/User/:Id', UserController.Put);
-    app.delete('/User/:Id', UserController.Delete);
-}
