8fa3dc2e4a04778a698c611a70c6b278e344c00d
Fernando Araujo <developgadget@gmail.com>
2019-06-25 12:21:01 -0500

    Chat

--- Chat-Socket/public/js/Chat.js
+++ Chat-Socket/public/js/Chat.js
@@ -1,1 +1,13 @@
 const SocketIO = io();
+
+function ClickBtn() {
+    SocketIO.emit('chat', {
+        message: document.getElementById('msg').value,
+        user: document.getElementById('user').value
+    });
+}
+
+SocketIO.on('chat', function (data) {
+    console.log(data);
+    document.getElementById('tags').innerHTML += `<span class="tag is-info">${data.user}: ${data.message}</span>`;
+})
